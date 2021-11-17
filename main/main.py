bea96474051f5de70b0ab07c306c37854262ea72
DoubaCorp <93625561+Douba93@users.noreply.github.com>
2021-11-17 13:30:16 +0100

    :)

---
+++ main/main.py
@@ -0,0 +1,17 @@
+import gtts
+from playsound import playsound
+import random
+from time import sleep
+import secrets
+from os import system
+
+def sound():
+	text = input('Text to Sound.mp3 Converter >')
+	tts = gtts.gTTS(text, lang="fr") #you can use lang="en"
+	soundName = secrets.token_urlsafe(random.randrange(2, 6))
+	tts.save(f"{soundName}.mp3")
+	sleep(2)
+	playsound(f"{soundName}.mp3")
+
+sound()
+input()
